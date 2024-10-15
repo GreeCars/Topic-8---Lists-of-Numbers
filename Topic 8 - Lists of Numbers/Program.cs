d0140bf501bdb1054403b3d8f8add4adb833be47
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-10-15 09:37:43 -0400

    Working

--- Topic 8 - Lists of Numbers/Program.cs
+++ Topic 8 - Lists of Numbers/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+List<int> numbers = new List<int>(); // Creates an empty List
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Please enter a number: ");
+    numbers.Add(Convert.ToInt32(Console.ReadLine()));
+}
+Console.ReadLine(); // Keeps the program from ending
+Console.WriteLine("The largest number is " + numbers.Max());
+Console.WriteLine("The smallest number is " + numbers.Min());
+Console.WriteLine("The sum is " + numbers.Sum());
